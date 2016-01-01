a2769433f66df97d264ceec28874c0143a3e5a56
Renzo Carpio Ponce <me@renzocarpio.com>
2015-12-31 19:29:39 -0500

    se quito la carpeta ./node_modules y se ejecuto browserify con babelify

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
+/*
+ * Module dependencies
+ */
+
 import React from 'react';
-import PokeTable from './components/Poketable';
+import PokeTable from './components/PokeTable';
+
 var pokemons = [
-	{number: 1, name: 'Bulbasaur'},
-	{number: 2, name: 'Ivysaur'},
-	{number: 3, name: 'Venasaur'}
-];		
-React.render(<PokeTable pokemons={pokemons} />,document.getElementById('container'));  
+  { number: 1, name: 'Bulbasaur' },
+  { number: 2, name: 'Ivysaur' },
+  { number: 3, name: 'Venusaur' }
+];
+
+React.render(
+  <PokeTable pokemons={pokemons} /	>,
+  document.getElementById('container')
+);
